46bfe83a2a6eb310147ee4e6798f28e9d7b0e94e
KingofBeast <KingofBeast@TheD3vine.net>
2017-11-04 21:17:05 -0500

    Use premake5

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,23 @@
 -- bjam release address-model=32 runtime-link=static --with-system --with-thread --with-date_time --with-regex --with-serialization stage
 
-local osname = os.get()
+local osname = os.target()
 local dllname = osname
 
 if dllname == "windows" then
 	dllname = "win32"
 end
 
-local boost = {
-	["linux"] = "/media/jake/storage/Documents/Developer/boost_1_59_0/",
-	["windows"] = "D:/Documents/Developer/boost_1_59_0",
-}
-local boostinclude = boost[osname]
-
 solution "tmysql4"
-
 	language "C++"
 	location ( osname .."-".. _ACTION )
-	flags { "Symbols", "NoEditAndContinue", "NoPCH", "StaticRuntime", "EnableSSE" }
+	symbols "On"
+	editandcontinue "Off"
+	vectorextensions "SSE"
+	flags {"NoPCH", "StaticRuntime"}
 	targetdir ( "bin/" .. osname .. "/" )
-	includedirs { "include/GarrysMod", "include/" .. osname, boostinclude }
+	includedirs { "include/GarrysMod", "include/" .. osname, "include/boost" }
 	platforms{ "x32" }
-	libdirs { "library/" .. osname, boostinclude .. "/stage/lib" }
+	libdirs { "library/" .. osname }
 
 	targetprefix ("gmsv_")
 	targetname(solution().name)
@@ -40,11 +36,12 @@ solution "tmysql4"
 	}
 	
 	configuration "Release"
+		optimize "On"
+		floatingpoint "Fast"
 		if osname == "linux" then
 			buildoptions { "-std=c++0x -pthread -Wl,-z,defs" }
 		end
 		defines { "NDEBUG" }
-		flags{ "Optimize", "FloatFast" }
 	
 	project "tmysql4"
 		defines { "GMMODULE", "ENABLE_QUERY_TIMERS" }
