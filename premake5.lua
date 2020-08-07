730fcdab9f2d58ab85e4f688f6e20a82ba66cd0f
KingofBeast <KingofBeast@TheD3vine.net>
2020-08-06 22:10:32 -0500

    Add x64 libs and update premake (x64 dlls crash atm)

--- premake5.lua
+++ premake5.lua
@@ -3,57 +3,61 @@
 local osname = os.target()
 local dllname = osname
 
-if dllname == "windows" then
-	dllname = "win32"
-end
-
 solution "tmysql4"
 
 	language "C++"
 	location ( osname .."-".. _ACTION )
 	
-	flags {"NoPCH"}
-	symbols"On"
-	editandcontinue"Off"
-	vectorextensions"SSE"
-	staticruntime"Off"
+	flags { "NoPCH" }
+	symbols "On"
+	editandcontinue "Off"
+	vectorextensions "SSE"
+	staticruntime "Off"
 	
-	targetdir ( "bin/" .. osname .. "/" )
+	targetdir ("bin/" .. osname .. "/")
 	includedirs { "include/GarrysMod", "include/" .. osname, "include/asio/asio/include" }
-	platforms{ "x86","x86_64" }
-	libdirs { "library/" .. osname }
+	platforms { "x86","x64" }
 
-	targetprefix ("gmsv_")
-	targetname(solution().name)
-	targetsuffix("_" .. dllname)
+	targetprefix "gmsv_"
+	targetname (solution().name)
 	targetextension ".dll"
 
 	if osname == "windows" then
 		links { "mariadbclient" }
 	elseif osname == "linux" then
 		links { "mariadbclient", "rt" }
-	else error( "unknown os: " .. osname ) end
+	else error("unknown os: " .. osname) end
 	
-	configurations
-	{ 
-		"Release"
-	}
+	configurations { "Release" }
 	
 	configuration "Release"
-		if osname == "linux" then
+		if (osname == "linux") then
 			buildoptions { "-std=c++0x -pthread -Wl,-z,defs" }
 		end
+
 		defines { "NDEBUG" }
-		optimize"On"
-		floatingpoint"Fast"
+		optimize "On"
+		floatingpoint "Fast"
 	
 	project "tmysql4"
 		defines { "GMMODULE", "ENABLE_QUERY_TIMERS" }
 		files { "src/**.*", "include/GarrysMod/**.*", "include/" .. osname .. "/**.*" }
 		kind "SharedLib"
 		
-		filter'platforms:x86'
-			architecture'x86'
-		filter'platforms:x64'
-			architecture'x86_64'
-		filter{}
+		filter "platforms:x86"
+			architecture "x86"
+			libdirs { "library/x86" }
+			if dllname == "windows" then
+				targetsuffix "_win32"
+			else
+				targetsuffix ("_" .. dllname)
+			end
+		filter "platforms:x64"
+			architecture "x86_64"
+			libdirs { "library/x64" }
+			if dllname == "windows" then
+				targetsuffix "_win64"
+			else
+				targetsuffix ("_" .. dllname)
+			end
+		filter { }
