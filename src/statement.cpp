358ab49d0f26a361ba2801374e0861fea21f911c
darkjacky <jackykoning@zonnet.nl>
2020-08-22 19:15:36 +0200

    Update statement.cpp

--- src/statement.cpp
+++ src/statement.cpp
@@ -102,7 +102,7 @@ int PStatement::lua___gc(lua_State* state)
 
 int PStatement::lua__tostring(lua_State* state)
 {
-	LUA->PushString("[Tmysql4 prepared statement]");
+	LUA->PushString("[Tmysql prepared statement]");
 	return 1;
 }
 
